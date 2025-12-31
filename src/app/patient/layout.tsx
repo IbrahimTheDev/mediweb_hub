7aba20c5c7b12cbc9364a2259bdcab0550a1f06d
MUZAMMIL IBRAHIM <202417bh043@wilp.bits-pilani.ac.in>
2025-12-31 11:24:07 +0000

    delete it complete everything of this function (_for element <LinkCompon

--- src/app/patient/layout.tsx
+++ src/app/patient/layout.tsx
@@ -128,12 +128,6 @@ export default function PatientLayout({
               <DropdownMenuSeparator />
               <DropdownMenuItem asChild>
                 <Link href="/profile">
-                  <User className="mr-2 h-4 w-4" />
-                  <span>Profile</span>
-                </Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem asChild>
-                <Link href="/profile">
                   <Settings className="mr-2 h-4 w-4" />
                   <span>Settings</span>
                 </Link>
